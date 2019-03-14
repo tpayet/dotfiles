454f4cdb9671dca4aaf0e82b121ee9e5dbe5fa40
Thomas Payet <thomas@meilisearch.com>
2019-03-14 11:17:42 +0100

    Add simple install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+files="alias tmux.conf vimrc zshrc"
+
+for file in $files ; do
+    echo "Creating symlink for $file at $HOME/.$file";
+    ln -s $(pwd)/$file "$HOME/.$file";
+    if [[ $? -eq 1 ]]; then
+        echo "Replacing with newer version";
+        ln -sf $(pwd)/$file "$HOME/.$file";
+    fi
+    echo "---"
+done
+
